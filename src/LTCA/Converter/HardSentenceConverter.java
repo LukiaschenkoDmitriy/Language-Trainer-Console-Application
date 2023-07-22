b4fd8105dbbadf55269a5a669be596703ad0113c
Dmitriy <lukiaschenkoff@gmail.com>
2023-07-22 18:12:25 +0300

    Implementation 'HardSentenceConverter' and 'IsNotHardSentenceException' classes

---
+++ src/LTCA/Converter/HardSentenceConverter.java
@@ -0,0 +1,14 @@
+package LTCA.Converter;
+
+public class HardSentenceConverter extends AbstractConverter {
+    private static HardSentenceConverter SINGLETON = null;
+    public final static String SUB_STRING = "!";
+    public final static boolean IN_END = false;
+    private HardSentenceConverter() {
+        super(SUB_STRING, IN_END);
+    }
+    public static HardSentenceConverter getSingleton() {
+        if (SINGLETON == null) SINGLETON = new HardSentenceConverter();
+        return SINGLETON;
+    }
+}
---
+++ src/LTCA/Exception/IsNotHardSentenceException.java
@@ -0,0 +1,13 @@
+package LTCA.Exception;
+
+import LTCA.Converter.HardSentenceConverter;
+
+public class IsNotHardSentenceException extends AbstractIsNotException {
+    private static String MESSAGE = "is not hard sentence";
+    private static String SUB_STRING = HardSentenceConverter.SUB_STRING;
+    private static String COMPONENT_NAME = "hard sentence";
+    private static boolean IN_END = HardSentenceConverter.IN_END;
+    public IsNotHardSentenceException(IsNotExceptionInfo info) {
+        super(info, MESSAGE, COMPONENT_NAME, SUB_STRING, IN_END);
+    }
+}
