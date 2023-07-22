b7f4223fa8669c1e55e46f73278685f7e15f3492
Dmitriy <lukiaschenkoff@gmail.com>
2023-07-22 18:17:03 +0300

    Implementation 'TranslateSentenceConverter' and 'IsNotTranslateSentenceException' components

---
+++ src/LTCA/Converter/TranslateSentenceConverter.java
@@ -0,0 +1,16 @@
+package LTCA.Converter;
+
+public class TranslateSentenceConverter extends AbstractConverter {
+    private static TranslateSentenceConverter SINGLETON = null;
+    public final static String SUB_STRING = "$";
+    public final static boolean IN_END = false;
+
+    private TranslateSentenceConverter () {
+        super(SUB_STRING, IN_END);
+    }
+
+    public static TranslateSentenceConverter getSingleton() {
+        if (SINGLETON == null) SINGLETON = new TranslateSentenceConverter();
+        return SINGLETON;
+    }
+}
---
+++ src/LTCA/Exception/IsNotTranslateSentenceException.java
@@ -0,0 +1,13 @@
+package LTCA.Exception;
+
+import LTCA.Converter.TranslateSentenceConverter;
+
+public class IsNotTranslateSentenceException extends AbstractIsNotException {
+    private static String MESSAGE = "is not translate sentence";
+    private static String SUB_STRING = TranslateSentenceConverter.SUB_STRING;
+    private static String COMPONENT_NAME = "TranslateSentence";
+    private static boolean IN_END = TranslateSentenceConverter.IN_END;
+    public IsNotTranslateSentenceException(IsNotExceptionInfo info) {
+        super(info, MESSAGE, COMPONENT_NAME, SUB_STRING, IN_END);
+    }
+}
