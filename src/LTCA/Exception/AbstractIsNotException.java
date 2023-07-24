a08ddc74739c347e97012760eb26fc5a56dc6ce3
Dmitriy <lukiaschenkoff@gmail.com>
2023-07-24 19:15:59 +0300

    Changing the exception message format in the 'AbstractIsNotException'

--- src/LTCA/Exception/AbstractIsNotException.java
+++ src/LTCA/Exception/AbstractIsNotException.java
@@ -1,7 +1,7 @@
 package LTCA.Exception;
 
 public abstract class AbstractIsNotException extends Exception {
-    private static final String FORMAT_MESSAGE = "Argument \"%s\" in %s \"%s\" %s \n\"%s\" %s \n%s";
+    private static final String FORMAT_MESSAGE = "\nThe \"%s\" argument - \"%s\" in the %s class \"%s\" %s \n%s";
     private static final String FORMAT_HELP_MESSAGE = "The %s must have the \"%s\" character on the %s of the line";
 
     public AbstractIsNotException(
@@ -14,10 +14,9 @@ public abstract class AbstractIsNotException extends Exception {
         super(String.format(
                 FORMAT_MESSAGE,
                 info.nameArgument,
+                info.content,
                 info.nameOperation,
                 info.nameClass,
-                message,
-                info.content,
                 message,
                 String.format(FORMAT_HELP_MESSAGE, componentName, componentSubString, !inEnd ? "beginning" : "end"))
         );
